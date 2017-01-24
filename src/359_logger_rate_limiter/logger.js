b8de38ae6329783be94602009fba7cdcafa0ac4d
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-01-24 08:37:33 -0800

    logger rate limiter

---
+++ src/359_logger_rate_limiter/logger.js
@@ -0,0 +1,41 @@
+/**
+ * Initialize your data structure here.
+ */
+var Logger = function() {
+    /*
+     * messages = { message: timestamp, ... }
+     */
+    this.messages = {};
+    this.TIME_THRESHOLD = 10
+};
+
+/**
+ * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+      If this method returns false, the message will not be printed.
+      The timestamp is in seconds granularity. 
+ * @param {number} timestamp 
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function(timestamp, message) {
+    if ( !this.messages.hasOwnProperty(message) ) {
+            this.messages[message] = timestamp;
+            return true;
+        }
+        
+    if (
+            this.messages.hasOwnProperty(message) && 
+            timestamp - this.messages[message] >= this.TIME_THRESHOLD
+        ) {
+            this.messages[message] = timestamp;
+            return true;
+        }
+    
+    return false
+};
+
+/** 
+ * Your Logger object will be instantiated and called as such:
+ * var obj = Object.create(Logger).createNew()
+ * var param_1 = obj.shouldPrintMessage(timestamp,message)
+ */
