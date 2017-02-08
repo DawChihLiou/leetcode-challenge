c8c5cb0b783ed5a1306fd77f6a108842d3a788b4
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-02-08 16:17:54 +0800

    258 add digit O(1) solution

---
+++ src/258_add_digits/add-digits-constant-runtime.js
@@ -0,0 +1,9 @@
+/**
+ * See digital roots formula: https://en.wikipedia.org/wiki/Digital_root
+ * 
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    return num === 0 ? 0 : (num % 9 === 0 ? 9 : num % 9);
+};
