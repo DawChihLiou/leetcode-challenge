24a60bd531dccfe676d0e8360b4a2b7eacbb8e95
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-02-20 11:18:03 +0800

    6 zigzag conversion

---
+++ src/006_zigzag_conversion/zigzag-conversion.js
@@ -0,0 +1,44 @@
+/**
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number 
+ * of rows like this: (you may want to display this pattern in a fixed font for 
+ * better legibility)
+ *
+ *  P   A   H   N
+ *  A P L S I I G
+ *  Y   I   R
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number 
+ * of rows:
+ *
+ *  string convert(string text, int nRows);
+ *  convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR"
+ *
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    // return original string if can't zigzag
+    if (numRows === 1 || s.length < numRows) return s;
+    
+    let rows = []
+    let converted = '';
+    let reverse = false;
+    let count = 0
+    
+    // prepare rows
+    for (let i = 0; i < numRows; i++) rows[i] = [];
+    
+    // reverse the push flow when reaching turning points
+    for (let i = 0; i < s.length; i++) {
+	rows[count].push(s[i]);
+        reverse ? count-- : count++;
+        if (count === numRows - 1 || count === 0) reverse = !reverse;
+    }
+    
+    // concat rows in order
+    for (const r of rows) converted += r.join('');
+    
+    return converted;
+};
