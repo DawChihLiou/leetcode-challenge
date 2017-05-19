dceecccf6367c08a4a90af7a461c2ba14e36c539
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-05-19 16:29:04 +0800

    561 add comment

--- src/561_array_partition_I/array-partition.js
+++ src/561_array_partition_I/array-partition.js
@@ -17,6 +17,7 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
+    // sort and sum up the minimum of the pairs
     return nums
         .sort((a, b) => a - b)
         .reduce((sum, cur, i) => (i % 2 === 0) ? sum += cur : sum, 0);
