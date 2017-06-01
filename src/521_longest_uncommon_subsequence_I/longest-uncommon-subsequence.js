05a54b38b16c5dbe8a7d8db0a2ed5959566b44a1
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-06-01 11:29:19 +0800

    update comment in 521 longest uncommon subsquecne I

--- src/521_longest_uncommon_subsequence_I/longest-uncommon-subsequence.js
+++ src/521_longest_uncommon_subsequence_I/longest-uncommon-subsequence.js
@@ -30,7 +30,7 @@
  * @return {number}
  */
 var findLUSlength = function(a, b) {
-    // identical strings doesn't have the uncommon subsequence
+    // identical strings don't have uncommon subsequence
     if (a === b) return -1;
     // the longer string itself is the longest uncommon subsequence
     else return Math.max(a.length, b.length);
