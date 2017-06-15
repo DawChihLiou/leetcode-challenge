55e6808c6055c6021945979af40b5af287fd9999
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-06-15 09:45:24 +0200

    447 number of boomerangs

---
+++ src/447_number_of_boomerangs/count-boomerangs.js
@@ -0,0 +1,43 @@
+/**
+ * Given n points in the plane that are all pairwise distinct, a "boomerang" is 
+ * a tuple of points (i, j, k) such that the distance between i and j equals the 
+ * distance between i and k (the order of the tuple matters).
+ *
+ * Find the number of boomerangs. You may assume that n will be at most 500 and 
+ * coordinates of points are all in the range [-10000, 10000] (inclusive).
+ *
+ * Example:
+ *  Input:
+ *    [[0,0],[1,0],[2,0]]
+ *
+ *  Output:
+ *    2
+ *
+ *  Explanation:
+ *    The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+ *
+ * @param {number[][]} points
+ * @return {number}
+ */
+var numberOfBoomerangs = function(points) {
+    let count = 0;
+    
+    for (let i = 0; i < points.length; i++) {
+        const memory = {};
+    
+        for (let j = 0; j < points.length; j++) {
+            if (i === j) continue;
+            
+            // search points with identical distance in memory and count tuple
+            const dist = Math.pow(
+                points[i][0] - points[j][0],2) + Math.pow(points[i][1] - points[j][1], 
+                2
+            );
+            
+            if (memory[dist]) count += memory[dist] * 2;
+            memory[dist] ? memory[dist] += 1 : memory[dist] = 1;
+        }
+    }
+
+    return count;
+};
